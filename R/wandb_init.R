6a3e60adef8c350bcaa9977f8f2b8e1fc59daa3c
Arron Lcey <arronslacey@gmail.com>
2021-10-28 00:26:07 +0100

    added documentation to wand_init and wand_agent

--- R/wandb_init.R
+++ R/wandb_init.R
@@ -1,4 +1,10 @@
-#' Title
+#' wandb_init
+#'
+#' Starts a new run to track and log to W&B.
+#'
+#' wandb_init spawns a new background process to log data to a run, and it also syncs data to wandb_ai by default so you can see live visualizations. Call wandb_init to start a run before logging data with wandb_log.
+#' wandb_init returns a run object, and you can also access the run object with wandb.run.
+#' At the end of your script, we will automatically call wandb_finish to finalize and cleanup the run. However, if you call wandb_init from a child process, you must explicitly call wandb_finish at the end of the child process.
 #'
 #' @param project
 #' @param entity
@@ -22,7 +28,7 @@ wandb_init <- function(project=NULL,learning_rate=NULL,architecture=NULL,
                        epoch= NULL,
                        batch_size= NULL,
                        dataset=NULL
-                       ){
+                       ) {
   wandb$init(project=project, config=dict(list(learning_rate=learning_rate,
                                                architecture=architecture,
                                                layer_1 = layer_1,
