efb6cad0e704440a4883a58b1160ff7c5b44a9d8
Arron Lcey <arronslacey@gmail.com>
2021-10-29 23:13:08 +0100

    updated documentation for wandb_login

--- R/wandb_log.R
+++ R/wandb_log.R
@@ -1,4 +1,10 @@
-#' Title
+#' wandb_log
+#'
+#' Logs a dictionary of data to the current run's history.
+#'
+#' @param acc accuracy
+#' @param loss loss
+#' @param epoch epoch
 #'
 #' @return
 #' @export
