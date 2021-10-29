9b8924b0d81cad83ce861194b45416e17072962d
Arron Lcey <arronslacey@gmail.com>
2021-10-29 23:06:23 +0100

    re-added hyperparams args to wandb_init

--- R/wandb_init.R
+++ R/wandb_init.R
@@ -31,9 +31,32 @@
 wandb_init <- function(project=NULL,
                        entity = NULL,
                        data=NULL,
-                       config=NULL
+                       config=NULL,
+                       learning_rate=NULL,architecture=NULL,
+                       layer_1= NULL,
+                       activation_1=NULL,
+                       dropout= NULL,
+                       layer_2= NULL,
+                       activation_2= NULL,
+                       optimizer= NULL,
+                       loss= NULL,
+                       metric= NULL,
+                       epoch= NULL,
+                       batch_size= NULL
                        ) {
 
-wandb$init(project=project, entity = entity, data = data, config = dict(config))
+wandb$init(project=project, entity = entity, data = data, config = dict(config),
+           learning_rate=learning_rate,
+           architecture=architecture,
+           layer_1 = layer_1,
+           activation_1 = activation_1,
+           dropout = dropout,
+           layer_2 = layer_2,
+           activation_2 = activation_2,
+           optimizer = optimizer,
+           loss = loss,
+           metric = metric,
+           epoch = epoch,
+           batch_size = batch_size)
 }
 
