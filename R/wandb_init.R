a6ce9f68569af5f874b03b353de6db681d76514c
Arron Lcey <arronslacey@gmail.com>
2021-10-30 23:47:11 +0100

    removed all config params from wandb_init

--- R/wandb_init.R
+++ R/wandb_init.R
@@ -30,34 +30,9 @@
 #' @examples
 wandb_init <- function(project=NULL,
                        entity = NULL,
-                       config=NULL,
-                       learning_rate=NULL,architecture=NULL,
-                       layer_1= NULL,
-                       activation_1=NULL,
-                       dropout= NULL,
-                       layer_2= NULL,
-                       activation_2= NULL,
-                       optimizer= NULL,
-                       loss= NULL,
-                       metric= NULL,
-                       epoch= NULL,
-                       batch_size= NULL,
-                       dataset = NULL
+                       config=NULL
                        ) {
 
-wandb$init(project=project, entity = entity, config = dict(config),
-           learning_rate=learning_rate,
-           architecture=architecture,
-           layer_1 = layer_1,
-           activation_1 = activation_1,
-           dropout = dropout,
-           layer_2 = layer_2,
-           activation_2 = activation_2,
-           optimizer = optimizer,
-           loss = loss,
-           metric = metric,
-           epoch = epoch,
-           batch_size = batch_size,
-           dataset = dataset)
+wandb$init(project=project, entity = entity, config = dict(config))
 }
 
